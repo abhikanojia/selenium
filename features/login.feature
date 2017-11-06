49aaa2818dec268a1e53d5206c72d96376903877
abhishekkanojia <abhishek.kanojia@vinsol.com>
2017-11-06 17:39:43 +0530

    Initialize repo

---
+++ features/login.feature
@@ -0,0 +1,8 @@
+Feature: User Login
+Scenario:
+  Given We navigate to the home page
+  And We enter email
+  And We enter password
+  Then We click login button
+  Then We should see dashboard
+
---
+++ features/step_definitions/login_steps.rb
@@ -0,0 +1,30 @@
+require 'rubygems'
+require 'selenium-webdriver'
+
+driver = Selenium::WebDriver.for :chrome
+
+Given("We navigate to the home page") do
+  driver.navigate.to 'http://ec2-52-15-34-101.us-east-2.compute.amazonaws.com/users/sign_in'
+end
+
+Then("We enter email") do
+  driver.find_element(:id, 'user_email').send_keys("abhishek.kanojia+1@vinsol.com")
+end
+
+Then("We enter password") do
+  driver.find_element(:id, 'user_password').send_keys("1111111")
+end
+
+Then("We click login button") do
+  driver.find_element(:class, 'submit').click
+end
+
+Then("We should see dashboard") do
+  wait = Selenium::WebDriver::Wait.new(:timeout => 10) # seconds
+  begin
+    element = wait.until { driver.find_element(:class => "positive") }
+    expect(element.text).to eq('Signed in successfully.')
+  ensure
+    driver.quit
+  end
+end
